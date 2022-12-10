85ba3ec88f4ccff0a670b8c8fd78c65513fe2f9d
Jordan White <jowhite@redhat.com>
2022-12-10 15:10:01 -0500

    Day 2: complete

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 /*
 --- Day 2: Rock Paper Scissors ---
 The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant Rock Paper Scissors tournament is already in progress.
@@ -26,5 +27,172 @@ In this example, if you were to follow the strategy guide, you would get a total
 
 What would your total score be if everything goes exactly according to your strategy guide?
 */
-pub fn solve() {
+
+const POINTS_WIN: u8 = 6;
+const POINTS_DRAW: u8 = 3;
+const POINTS_LOSS: u8 = 0;
+
+const POINTS_ROCK: u8 = 1;
+const POINTS_PAPER: u8 = 2;
+const POINTS_SCISSORS: u8 = 3;
+
+pub fn solve() -> (u32, u32) {
+    let input = include_str!("../input/day2/part1.txt");
+    (part1(input), part2(input))
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut total_score: u32 = 0;
+
+    for mut line in input.lines() {
+        let win_state = line.replace(" ", "").chars().last().unwrap();
+        line = line.trim_end_matches(win_state);
+        let opponent = line.replace(" ", "").chars().last().unwrap();
+
+        total_score += get_part2_round_score(opponent, win_state) as u32;
+    }
+
+    total_score
+
+}
+
+pub fn part1(input: &str) -> u32 {
+    let mut total_score: u32 = 0;
+
+    for mut line in input.lines() {
+        let player = line.replace(" ", "").chars().last().unwrap();
+        line = line.trim_end_matches(player);
+        let opponent = line.replace(" ", "").chars().last().unwrap();
+
+        total_score += get_part1_round_score(opponent, player) as u32;
+    }
+
+    total_score
+}
+
+pub fn get_part1_round_score(opponent: char, player: char) -> u8 {
+    let player_shape = get_player_shape(player);
+    get_player_win_score(&get_opponent_shape(opponent), &player_shape) + get_shape_score(&player_shape)
+}
+
+pub fn get_part2_round_score(opponent: char, win_state: char) -> u8 {
+    let player_shape = get_player_move_from_win_state(&get_opponent_shape(opponent), get_win_state_from_second_column(win_state));
+    get_player_win_score(&get_opponent_shape(opponent), &player_shape) + get_shape_score(&player_shape)
+}
+
+
+pub fn get_player_win_score(opponent: &str, player: &str) -> u8 {
+    return match (opponent, player) {
+        ("rock", "rock") => POINTS_DRAW,
+        ("rock", "paper") => POINTS_WIN,
+        ("rock", "scissors") => POINTS_LOSS,
+        ("paper", "rock") => POINTS_LOSS,
+        ("paper", "paper") => POINTS_DRAW,
+        ("paper", "scissors") => POINTS_WIN,
+        ("scissors", "rock") => POINTS_WIN,
+        ("scissors", "paper") => POINTS_LOSS,
+        ("scissors", "scissors") => POINTS_DRAW,
+        _ => unreachable!()
+    }
+}
+
+pub fn get_player_move_from_win_state(opponent: &str, win_state: u8) -> String {
+    return match (opponent, win_state) {
+        ("rock", POINTS_DRAW) => String::from("rock"),
+        ("rock", POINTS_WIN) => String::from("paper"),
+        ("rock", POINTS_LOSS) => String::from("scissors"),
+        ("paper", POINTS_LOSS) => String::from("rock"),
+        ("paper", POINTS_DRAW) => String::from("paper"),
+        ("paper", POINTS_WIN) => String::from("scissors"),
+        ("scissors", POINTS_WIN) => String::from("rock"),
+        ("scissors", POINTS_LOSS) => String::from("paper"),
+        ("scissors", POINTS_DRAW) => String::from("scissors"),
+        _ => unreachable!()
+    }
+}
+
+pub fn get_opponent_shape(c: char) -> String {
+    return match c {
+        'A' => String::from("rock"),
+        'B' => String::from("paper"),
+        'C' => String::from("scissors"),
+        _ => unreachable!()
+    }
+}
+
+pub fn get_player_shape(c: char) -> String {
+    return match c {
+        'X' => String::from("rock"),
+        'Y' => String::from("paper"),
+        'Z' => String::from("scissors"),
+        _ => unreachable!()
+    }
+}
+
+pub fn get_win_state_from_second_column(c: char) -> u8 {
+    return match c {
+        'X' => POINTS_LOSS,
+        'Y' => POINTS_DRAW,
+        'Z' => POINTS_WIN,
+        _ => unreachable!()
+    }
+}
+
+pub fn get_shape_score(shape: &str) -> u8 {
+    return match shape {
+        "rock" => POINTS_ROCK,
+        "paper" => POINTS_PAPER,
+        "scissors" => POINTS_SCISSORS,
+        _ => unreachable!()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_player_win_score_win() {
+        assert_eq!(get_player_win_score("rock", "paper"), POINTS_WIN);
+    }
+
+    #[test]
+    fn test_get_player_win_score_draw() {
+        assert_eq!(get_player_win_score("paper", "paper"), POINTS_DRAW);
+    }
+    
+    #[test]
+    fn test_get_player_win_score_loss() {
+        assert_eq!(get_player_win_score("scissors", "paper"), POINTS_LOSS);
+    }
+
+    #[test]
+    fn test_get_part1_round_score() {
+        assert_eq!(get_part1_round_score('A', 'Y'), 8);
+        assert_eq!(get_part1_round_score('B', 'X'), 1);
+        assert_eq!(get_part1_round_score('C', 'Z'), 6);
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = "A Y
+B X
+C Z";
+        assert_eq!(part1(input), 15);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "A Y
+B X
+C Z";
+        assert_eq!(part2(input), 12);
+    }
+
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve(), (8392, 10116));
+    }
 }
